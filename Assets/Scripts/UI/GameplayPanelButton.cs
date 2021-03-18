1b3a39e027be157f12aa6cba82733121b65076f5
Wafik DIDI <68503946+WafikDIDI@users.noreply.github.com>
2021-03-18 17:41:46 +0100

    UI Fixes

---
+++ Assets/Scripts/UI/GameplayPanelButton.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DivinityGaz.UI
+{
+    public class GameplayPanelButton : MonoBehaviour
+    {
+        [SerializeField] private GameplayPanelButtonsManager manager = null; 
+        [SerializeField] private Image backgroundEffect = null;
+
+        private void Awake ()
+        {
+            backgroundEffect.transform.DOScaleX(0f, 0f);
+        }
+
+        internal void EnableTween()
+        {
+            backgroundEffect.transform.DOScaleX(1f, manager.TweenDuration);
+        }
+
+        internal void DisableTween ()
+        {
+            backgroundEffect.transform.DOScaleX(0f, manager.TweenDuration);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/GameplayPanelButtonsManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DivinityGaz.UI
+{
+    public class GameplayPanelButtonsManager : MonoBehaviour
+    {
+        [SerializeField] private float tweenDuration = 0.7f;
+        internal float TweenDuration { get { return tweenDuration; } }
+
+        [SerializeField] private GameplayPanelButton lastButtonClicked;
+
+        private List<GameplayPanelButton> buttons = new List<GameplayPanelButton>();
+
+        private void Awake ()
+        {
+            foreach (Transform transform in transform)
+            {
+                if (transform.TryGetComponent(out GameplayPanelButton button))
+                {
+                    buttons.Add(button);
+                }
+            }
+
+            lastButtonClicked.EnableTween();
+        }
+
+        public void OnButtonClicked (GameplayPanelButton newButton)
+        {
+            if (newButton == lastButtonClicked) { return; }
+
+            lastButtonClicked.DisableTween();
+            newButton.EnableTween();
+            lastButtonClicked = newButton;
+        }
+    }
+}
